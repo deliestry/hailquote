ef0f94d019c65a29a9c1e015018b1d7114ffd1b0
HailQuote <hailquote@local.invalid>
2026-07-16 18:54:46 +0200

    Use official DELIESTRY logo on login

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "hailquote-v11";
+const CACHE = "hailquote-v12";
 const ASSETS = [
   "./",
   "./index.html",
@@ -6,6 +6,7 @@ const ASSETS = [
   "./app.js",
   "./manifest.webmanifest",
   "./icon.svg",
+  "./assets/deliestry-logo.jpg",
   "./vendor/jspdf.umd.min.js"
   ,"./vendor/supabase.js"
 ];
